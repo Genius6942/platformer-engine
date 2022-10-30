2075de7f071ba10c0167e6ca3e77dd236bff136d
Joshua Liu <supajoshjunk@gmail.com>
2022-10-30 16:25:17 +0000

    docs on gh pages?

--- src/index.ts
+++ src/index.ts
@@ -9,100 +9,105 @@ renderer
   .enablePixelated()
   .enablePhysics({});
 
-const images = await loadImages(
+loadImages(
   {
     playerLeft: playerLeftImg,
     playerRight: playerRightImg,
   },
   (loaded, total) => console.log("Image progress:", (loaded / total) * 100, "%")
-);
+).then((images) => {
+  const player = new ControlledBody({
+    x: 30,
+    y: 30,
+    width: 30,
+    height: 30,
+    image: images.playerLeft,
+    layer: 1,
+    wallJump: true,
+    update: (self) => {
+      if (
+        (self.keys.a || self.keys.left) &&
+        !(self.keys.d || self.keys.right)
+      ) {
+        self.image = images.playerLeft;
+      } else if (
+        !(self.keys.a || self.keys.left) &&
+        (self.keys.d || self.keys.right)
+      ) {
+        self.image = images.playerRight;
+      }
+    },
+  });
+  player.bindKeyboardControls({});
+  renderer.add(player);
+  renderer.camera.lock(player, { minYSpace: -Infinity });
 
-const player = new ControlledBody({
-  x: 30,
-  y: 30,
-  width: 30,
-  height: 30,
-  image: images.playerLeft,
-  layer: 1,
-  wallJump: true,
-  update: (self) => {
-    if ((self.keys.a || self.keys.left) && !(self.keys.d || self.keys.right)) {
-      self.image = images.playerLeft;
-    } else if (
-      !(self.keys.a || self.keys.left) &&
-      (self.keys.d || self.keys.right)
-    ) {
-      self.image = images.playerRight;
+  renderer.add(
+    new StaticBody({ x: 0, y: 500, width: 300, height: 100, color: "black" })
+  );
+
+  let dragging = false,
+    dragStart = { x: 0, y: 0 },
+    dragEnd = { x: 0, y: 0 };
+
+  renderer.addEventListener("mousedown", ({ clientX, clientY }) => {
+    dragging = true;
+    dragStart = dragEnd = { x: clientX, y: clientY };
+  });
+
+  window.addEventListener("mousemove", ({ clientX, clientY }) => {
+    if (dragging) {
+      dragEnd = { x: clientX, y: clientY };
     }
-  },
-});
-player.bindKeyboardControls({});
-renderer.add(player);
-renderer.camera.lock(player, { minYSpace: -Infinity });
+  });
 
-renderer.add(
-  new StaticBody({ x: 0, y: 500, width: 300, height: 100, color: "black" })
-);
+  window.addEventListener("mouseup", ({ clientX, clientY }) => {
+    if (dragging) {
+      dragEnd = { x: clientX, y: clientY };
+      dragging = false;
+      dragging = false;
 
-let dragging = false,
-  dragStart = { x: 0, y: 0 },
-  dragEnd = { x: 0, y: 0 };
+      dragStart.x += -renderer.width / 2 + renderer.camera.pos.x;
+      dragStart.y += -renderer.height / 2 + renderer.camera.pos.y;
 
-renderer.addEventListener("mousedown", ({ clientX, clientY }) => {
-  dragging = true;
-  dragStart = dragEnd = { x: clientX, y: clientY };
-});
+      dragEnd.x += -renderer.width / 2 + renderer.camera.pos.x;
+      dragEnd.y += -renderer.height / 2 + renderer.camera.pos.y;
 
-window.addEventListener("mousemove", ({ clientX, clientY }) => {
-  if (dragging) {
-    dragEnd = { x: clientX, y: clientY };
-  }
-});
+      const x =
+        Math.min(dragStart.x, dragEnd.x) +
+        Math.abs(dragEnd.x - dragStart.x) / 2;
+      const y =
+        Math.min(dragStart.y, dragEnd.y) +
+        Math.abs(dragEnd.y - dragStart.y) / 2;
+      const width = Math.abs(dragEnd.x - dragStart.x);
+      const height = Math.abs(dragEnd.y - dragStart.y);
 
-window.addEventListener("mouseup", ({ clientX, clientY }) => {
-  if (dragging) {
-    dragEnd = { x: clientX, y: clientY };
-    dragging = false;
-    dragging = false;
+      renderer.add(new StaticBody({ x, y, width, height, color: "black" }));
+    }
+  });
 
-    dragStart.x += -renderer.width / 2 + renderer.camera.pos.x;
-    dragStart.y += -renderer.height / 2 + renderer.camera.pos.y;
+  const animationLoop = () => {
+    renderer.update();
+    if (player.y - player.height / 2 > renderer.height) {
+      player.v.y = 0;
+      player.v.x = 0;
+      player.x = 30;
+      player.y = 30;
+    }
+    renderer.render();
 
-    dragEnd.x += -renderer.width / 2 + renderer.camera.pos.x;
-    dragEnd.y += -renderer.height / 2 + renderer.camera.pos.y;
+    if (dragging) {
+      renderer.ctx.fillStyle = "blue";
+      renderer.ctx.fillRect(
+        dragStart.x,
+        dragStart.y,
+        dragEnd.x - dragStart.x,
+        dragEnd.y - dragStart.y
+      );
+    }
 
-    const x =
-      Math.min(dragStart.x, dragEnd.x) + Math.abs(dragEnd.x - dragStart.x) / 2;
-    const y =
-      Math.min(dragStart.y, dragEnd.y) + Math.abs(dragEnd.y - dragStart.y) / 2;
-    const width = Math.abs(dragEnd.x - dragStart.x);
-    const height = Math.abs(dragEnd.y - dragStart.y);
-
-    renderer.add(new StaticBody({ x, y, width, height, color: "black" }));
-  }
-});
-
-const animationLoop = () => {
-  renderer.update();
-  if (player.y - player.height / 2 > renderer.height) {
-    player.v.y = 0;
-    player.v.x = 0;
-    player.x = 30;
-    player.y = 30;
-  }
-  renderer.render();
-
-  if (dragging) {
-    renderer.ctx.fillStyle = "blue";
-    renderer.ctx.fillRect(
-      dragStart.x,
-      dragStart.y,
-      dragEnd.x - dragStart.x,
-      dragEnd.y - dragStart.y
-    );
-  }
+    requestAnimationFrame(animationLoop);
+  };
 
   requestAnimationFrame(animationLoop);
-};
-
-requestAnimationFrame(animationLoop);
+});
